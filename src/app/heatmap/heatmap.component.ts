187f85a6f9df1327d6430e0df2f843a4ff2a4c5d
Toan Phung <toan.phung@proteo.info>
2024-06-10 13:17:06 +0100

    update

--- src/app/heatmap/heatmap.component.ts
+++ src/app/heatmap/heatmap.component.ts
@@ -88,8 +88,6 @@ export class HeatmapComponent {
     }
     for (const s of this.sessionList) {
       if (this.settings.settings.labelMap[s]) {
-        temp.x.push(this.settings.settings.labelMap[s])
-      } else {
         temp.x.push(s)
       }
     }
@@ -139,6 +137,7 @@ export class HeatmapComponent {
     if (this.form.value["sortBy"] !== "" && this.form.value["sortBy"]){
       this.sortHeatmapBySession(this.form.value["sortBy"], temp)
     }
+    temp.x.map((a: string) => this.settings.settings.labelMap[a] ? this.settings.settings.labelMap[a] : a)
     this.graphData = [temp]
     this.graphLayout.margin.l = longestTextSize * 8
     // calculate width of graph based on number of sessions and height of graph based on the number of proteins found
